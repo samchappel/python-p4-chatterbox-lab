a41dbd5dff00c0e896eaa583be9b9d17b8dce1cc
Sam Chappel <m.samchappel@gmail.com>
2023-03-15 09:29:42 -0700

    lab completed

--- server/app.py
+++ server/app.py
@@ -14,13 +14,80 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method =='GET':
+        messages = Message.query.order_by('created_at').all()
+        # retrieve all Message objects from the database and order them by created_at field
 
-@app.route('/messages/<int:id>')
+        response = make_response(
+            # create a JSON response containing a list of dictionaries with each Message object's attributes
+            jsonify([message.to_dict() for message in messages]),
+            200,
+            # and set the HTTP status code to 200 (OK)
+        )
+    elif request.method == 'POST':
+        data = request.get_json()
+        # retrieve the JSON data from the request body
+        message = Message(
+            body=data['body'],
+            username=data['username']
+            # create a new Message object with the body and username attributes from the JSON data
+        )
+
+        # add the new Message object to the session and commit changes to the database
+        db.session.add(message)
+        db.session.commit()
+
+        response = make_response(
+            jsonify(message.to_dict()),
+            # create a JSON response containing the new Message object's attributes
+            201,
+            # and set the HTTP status code to 201 (Created)
+        )
+
+    # return the response object
+    return response
+
+
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    # retrieve the Message object with the given id from the database
+
+    if request.method == 'PATCH':
+        data = request.get_json()
+        # retrieve the JSON data from the request body
+        for attr in data:
+            setattr(message, attr, data[attr])
+        # update the Message object's attributes based on the JSON data
+
+        db.session.add(message)
+        db.session.commit()
+        # add the updated Message object to the session and commit changes to the database
+
+        response = make_response(
+            jsonify(message.to_dict()),
+            # create a JSON response containing the updated Message object's attributes
+            200,
+            # create a JSON response containing the updated Message object's attributes
+        )
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        # delete the Message object from the session and commit changes to the database
+
+        response = make_response(
+            jsonify({'deleted': True}),
+            # create a JSON response indicating that the Message object was deleted
+            200,
+            # and set the HTTP status code to 200 (OK)
+        )
+
+    return response
+    # return the response object
 
 if __name__ == '__main__':
     app.run(port=5555)
